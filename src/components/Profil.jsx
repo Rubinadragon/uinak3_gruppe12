8c2c30254e66f8bafcd7ffd499b10c613e140dff
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-04-03 15:27:40 +0200

    fetche enkeltlogg på profilside med map

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,6 +1,38 @@
+import { useParams } from "react-router";
+import { fetchLogByPerson } from "../sanity/loggServices";
+import { useEffect, useState } from "react";
+
 export default function Profil(){
+
+    const { profile } = useParams();
+    const [singleLogg, setSingleLogg] = useState([])
+
+    const getPersonBySlug = async (profile) => {
+        const data = await fetchLogByPerson(profile);
+        setSingleLogg(data);
+    }
+
+    useEffect(() => {
+        getPersonBySlug(profile)
+    }, [profile])
+
     return(
         <>
+        {
+            singleLogg?.map((loggRad) => (
+                <article key={loggRad._id}>
+                    <span>{loggRad.loggdato}</span>
+                    {
+                        loggRad.loggpersoner.map((loggPerson, index) => (
+                            <span key={`person_${index}`}>{loggPerson.personnavn}
+                            {loggRad.loggpersoner.length > index + 1 ? ", " : ""}</span>
+                        ))
+                    }
+                    <span>{loggRad.loggbeskrivelse}</span>
+                    <span>{loggRad.loggtimer}</span>
+                </article>
+            ))
+        }
             <article>
                 <h3>Navn</h3>
                 <p>Beskrivelse</p>
