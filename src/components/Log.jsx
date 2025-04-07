e2a7ed69de8b1f91772c7033f39a813614d9f656
JulieAnd3 <julia@hiof.no>
2025-04-07 15:36:44 +0200

    Stylet logg
    Skrevet stylingfil for logg og lagt på id på html elementene for å treffe med stylingen

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -3,15 +3,15 @@ import "../assets/styles/log.scss";
 export default function Log({loggRad}){
     return(
         <article>
-        <span>{loggRad.loggdato}</span>
+        <span id="loggdato">{loggRad.loggdato}</span>
         {
             loggRad.loggpersoner.map((loggPerson, index) => (
-                <span key={`person_${index}`}>{loggPerson.personnavn}
+                <span id="loggperson" key={`person_${index}`}>{loggPerson.personnavn}
                 {loggRad.loggpersoner.length > index + 1 ? ", " : ""}</span>
             ))
         }
-        <span>{loggRad.loggbeskrivelse}</span>
-        <span>{loggRad.loggtimer}</span>
+        <span id="loggbeskrivelse">{loggRad.loggbeskrivelse}</span>
+        <span id="loggtid">{loggRad.loggtimer}</span>
     </article>
 )
     
