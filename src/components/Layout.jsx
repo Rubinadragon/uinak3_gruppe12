ff195f7adf113fc49b2d3ba476773539b4c68e36
Rubinadragon <cecilive@hiof.no>
2025-04-08 19:18:58 +0200

    Sass på header og layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import Nav from "./Nav";
+import "../assets/styles/layout.scss"
 
 export default function Layout({children, persons}){
     return(
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "../assets/styles/nav.scss"
 
 export default function Nav({persons}){
     console.log(persons)
     return (
     <>
-        <h1><Link to="/">Test</Link></h1>
+        <h1><Link to="/">Gruppe 12</Link></h1>
         <ul>
             {
                 persons?.map((person) => (
